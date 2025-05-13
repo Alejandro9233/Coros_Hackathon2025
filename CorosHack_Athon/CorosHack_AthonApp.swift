843f2c4e93762c019e17a127556ec035cb66ed05
Alejandro9233 <a.barrera9233@gmail.com>
2025-05-12 22:55:45 -0600

    Initial commit

---
+++ CorosHack_Athon/CorosHack_AthonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CorosHack_AthonApp.swift
+//  CorosHack_Athon
+//
+//  Created by Alejandro  on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CorosHack_AthonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
